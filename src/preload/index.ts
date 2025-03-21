0a99c2c6f2832b1d5589a17a27d6e74d12e264f4
AnhNguyen <rosenguyen1027@gmail.com>
2025-03-21 22:58:08 +0200

    edited configuration

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,5 +1,5 @@
-import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
+import { contextBridge } from 'electron'
 
 // Custom APIs for renderer
 const api = {}
@@ -9,8 +9,9 @@ const api = {}
 // just add to the DOM global.
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
+    contextBridge.exposeInMainWorld('context', {
+      // TODO
+    })
   } catch (error) {
     console.error(error)
   }
